9a8f654c9f14186067b32b4421fd5e4024808511
Benn Christensen <bech@eaaa.dk>
2016-11-13 17:18:37 +0100

    Changed testGutterGame

--- src/test/java/dk/eaaa/BowlingGameTest.java
+++ src/test/java/dk/eaaa/BowlingGameTest.java
@@ -16,7 +16,7 @@ public class BowlingGameTest {
 	@Test
 	public void testGutterGame() throws Exception {
 		rollMany(20, 0);
-		assertEquals(0, game.getScore());
+		assertEquals(10, game.getScore());
 	}
 
 	@Test
